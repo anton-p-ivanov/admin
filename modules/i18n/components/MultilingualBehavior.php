601a148537dfcd386d7d6b358d969be16d4053e9
Anton Ivanov <yapi68@ya.ru>
2018-02-08 15:22:12 +0300

    Updated

--- modules/i18n/components/MultilingualBehavior.php
+++ modules/i18n/components/MultilingualBehavior.php
@@ -2,9 +2,13 @@
 namespace i18n\components;
 
 use i18n\models\Language;
+use yii\db\ActiveQuery;
 
 /**
  * Class MultilingualBehavior
+ *
+ * @property \yii\db\ActiveRecord $owner
+ *
  * @package i18n\components
  */
 class MultilingualBehavior extends \omgdef\multilingual\MultilingualBehavior
@@ -28,5 +32,17 @@ class MultilingualBehavior extends \omgdef\multilingual\MultilingualBehavior
         if (!$this->languages) {
             $this->languages = Language::find()->select('code')->column();
         }
+    }
+
+    /**
+     * Relation to model translation
+     * @param string $language
+     * @return ActiveQuery
+     */
+    public function getTranslation($language = null)
+    {
+        $language = $language ?: $this->getCurrentLanguage();
+        return $this->owner->hasOne($this->langClassName, [$this->langForeignKey => $this->owner->primaryKey()[0]])
+            ->where([sprintf('%s.[[%s]]', $this->tableName, $this->languageField) => $language]);
     }
 }
