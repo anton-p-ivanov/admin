054e34e8341ed922dfe8e6c619acec9872065866
Anton Ivanov <yapi68@ya.ru>
2018-02-08 15:30:41 +0300

    Module rules auto-collecting

--- config/rules.php
+++ config/rules.php
@@ -3,14 +3,24 @@
  * UrlManager rules for main module.
  */
 
+$rules = [];
+$path = realpath(__DIR__ . '/../modules');
+
+foreach(new DirectoryIterator($path) as $item) {
+    if ($item->isDir() && !$item->isDot()) {
+        $filename = $item->getPathname() . '/config/rules.php';
+        if (file_exists($filename)) {
+            $rules = \yii\helpers\ArrayHelper::merge($rules, require $filename);
+        }
+    }
+}
+
 return \yii\helpers\ArrayHelper::merge(
-    require __DIR__ . "/../modules/accounts/config/rules.php",
-    require __DIR__ . "/../modules/mail/config/rules.php",
-    require __DIR__ . "/../modules/forms/config/rules.php",
-    require __DIR__ . "/../modules/storage/config/rules.php",
-    require __DIR__ . "/../modules/users/config/rules.php",
+    $rules,
     [
         'api/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/api/<controller>/<action>',
+        'admin/<module:\w+>' => '<module>/admin',
+        'admin/<module:\w+>/<controller:\w+>' => '<module>/admin/<controller>',
         'admin/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/admin/<controller>/<action>',
         '<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
         '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
