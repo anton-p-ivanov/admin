90dbec06ea3c87a68c58d8e104806d485670d7ce
Anton Ivanov <yapi68@ya.ru>
2018-03-12 15:15:19 +0300

    Remove unnecessary params

--- modules/training/modules/admin/views/questions/copy.php
+++ modules/training/modules/admin/views/questions/copy.php
@@ -2,8 +2,7 @@
 /**
  * @var \yii\web\View $this
  * @var \training\modules\admin\models\Question $model
- * @var string $course_uuid
  */
 ?>
 
-<?= $this->render('.form.php', ['model' => $model, 'title' => 'Copy question', 'course_uuid' => $course_uuid]); ?>
+<?= $this->render('.form.php', ['model' => $model, 'title' => 'Copy question']); ?>
